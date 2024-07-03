9afde1422a799d307477bd86c6e110cae647a83d
ccjustin <ccjustin@umich.edu>
2024-07-03 17:54:33 -0500

    users route added

---
+++ next-app/app/page.tsx
@@ -0,0 +1,8 @@
+// landing page
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main><h1>hi world</h1></main>
+      )
+}
---
+++ next-app/app/users/page.tsx
@@ -0,0 +1,11 @@
+// use "rafce" extension to generate a React Arrow Function Component 
+// css files cannot be created within this route
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>UserPage</div>
+  )
+}
+
+export default UserPage
